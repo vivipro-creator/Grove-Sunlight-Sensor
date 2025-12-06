47bb3e065123c397fe5874f82e0f029e3007d2e5
vivipro-creator <viviana.probst@ph-weingarten.de>
2025-12-06 12:08:01 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,23 @@
-//% color=#ffaa00 icon="\uf185" weight=80 block="Grove-Sunlight-Sensor"
-namespace Grove-Sunlight-Sensor {
+namespace SunlightSensor {
 
-    //% block="Initialisiere Sonnenlichtsensor" weight=100 blockGap=8
-    export function initSunlight(): void {
+    //% blockId=sunlight_init
+    //% block="Initialisiere Sonnenlichtsensor"
+    export function init(): void {
         SI1151.initSunlight();
     }
 
-    //% block="Licht Intensität (lm)" weight=90 blockGap=8
-    export function lightIntensity(): number {
+    //% blockId=sunlight_get_lux
+    //% block="Licht Intensität (lm)"
+    //% blockNamespace=SunlightSensor
+    export function getLightIntensityLm(): number {
         return SI1151.getHalfWord_Visible();
     }
 
-    //% block="zeige Lichtintensität auf LED" weight=80
-    export function showLightOnLED(): void {
-        basic.showNumber(lightIntensity());
+    //% blockId=sunlight_show_lux
+    //% block="zeige Lichtintensität (lm) auf LEDs"
+    //% blockNamespace=SunlightSensor
+    export function showLightOnLeds(): void {
+        const value = getLightIntensityLm();
+        basic.showNumber(value);
     }
 }
-
