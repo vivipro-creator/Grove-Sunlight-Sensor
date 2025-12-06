5b67f0d6736c2fd3d2fe8410b964cf6cc42682b5
vivipro-creator <viviana.probst@ph-weingarten.de>
2025-12-06 11:34:07 +0100

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,19 @@
+//% color=#ffaa00 icon="\uf185" weight=80 block="Sunlight-Sensor"
+namespace Sunlight-Sensor {
+
+    //% block="Initialisiere Sonnenlichtsensor" weight=100 blockGap=8
+    export function initSunlight(): void {
+        SI1151.initSunlight();
+    }
+
+    //% block="Licht Intensität (lm)" weight=90 blockGap=8
+    export function lightIntensity(): number {
+        return SI1151.getHalfWord_Visible();
+    }
+
+    //% block="zeige Lichtintensität auf LED" weight=80
+    export function showLightOnLED(): void {
+        basic.showNumber(lightIntensity());
+    }
+}
+
